170ea276dcea6e2dd71c483178370c159363bba3
maks228998 <78788414+maks228998@users.noreply.github.com>
2021-03-02 11:42:09 +0600

    Add files via upload

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<cstdlib>
+#include "my_lib.h"
+
+int main() {
+	printf("Calculating factorial\n");
+	int n = input_int_number();
+	int factorial = 1;
+
+	for (int counter = 1; counter <= n; counter++) {
+		factorial = factorial * counter;
+	}
+
+	printf("Calculated factorial: %d", factorial);
+}
